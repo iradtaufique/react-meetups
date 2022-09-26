a09cbce650dab25208c7da4decd4b2a42ffcc8f4
iradtaufique <iradukunda.ta@gmail.com>
2022-09-26 08:36:49 +0200

    added styles and some comments on App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { Route, Switch } from "react-router-dom";
-import AllMeetupPage from "./pages/AllMeetups";
-import NewMeetupPage from "./pages/NewMeetup";
-import FavoritePage from "./pages/Favorites";
-import MainNavigation from './components/layout/MainNavigation'
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      {/* Main Navigation that contain Project navigation */}
       <MainNavigation />
+      {/* Switch used to switch among routes */}
       <Switch>
-        <Route path="/" exact={true}>
-          <AllMeetupPage />
+        {/* defining the routes with Route */}
+        <Route path='/' exact>
+          <AllMeetupsPage />
         </Route>
-
-        <Route path="/new-meetup">
+        <Route path='/new-meetup'>
           <NewMeetupPage />
         </Route>
-        <Route path="/favorite">
-          <FavoritePage />
+        <Route path='/favorites'>
+          <FavoritesPage />
         </Route>
       </Switch>
     </div>
