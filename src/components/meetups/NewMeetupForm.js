9426cb258105d9b4d4333948301640cc116fcb58
iradtaufique <iradukunda.ta@gmail.com>
2022-09-26 16:05:50 +0200

    added data into firebase database

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,29 +1,58 @@
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
+import { useRef } from "react";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    // preventDefault() will prevent form defaul working and do our own
+    event.preventDefault();
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    props.onAddMeetup(meetupData)
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           {/* htmlFor is the some as for atribute on label in usual HTML */}
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
 
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
 
         <div className={classes.control}>
           <label htmlFor="address">Meetup Address </label>
-          <input type="text" required id="affress" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
 
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea rows="5" required></textarea>
+          <textarea
+            rows="5"
+            required
+            ref={descriptionInputRef}
+            id="description"
+          ></textarea>
         </div>
 
         <div className={classes.actions}>
