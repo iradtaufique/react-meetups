427abb8c2eb580a191bab6bccc62ff5c43dfd289
iradtaufique <iradukunda.ta@gmail.com>
2022-10-04 13:02:38 +0200

    retrieve favorite on favorite page and add badge to show favorites numbers

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,22 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavoritesContext from "../store/favarites-context";
+
 function FavoritePage() {
-    return <div>Favorite Page</div>;
+  const favoriteCtx = useContext(FavoritesContext);
+
+  let content;
+  if (favoriteCtx.totalFavorites === 0) {
+    content = <p>No Fovorite yet. start Add Some?</p>;
+  } else {
+    content = <MeetupList meetups={favoriteCtx.favorites} />;
   }
-  
-  export default FavoritePage;
-  
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
+}
+
+export default FavoritePage;
